c7bd5a581e3b249a179b1f658b840f19d1ec226f
Tobi <tobiolajide@github.com>
2018-03-03 14:40:02 +0000

    updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
+
 (function ($) {
 	'use strict';
-	$("#nav ul li a[href*='#']").click(function () {
+	$("#nav ul li a[href*='#'], a[href*='#'] ").click(function () {
 		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
 			var target = $(this.hash);
 			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -12,6 +13,7 @@
 			}
 		}
 	});
+	
 	$(".navbar-nav li a").click(function (event) {
 		$(".navbar-collapse").collapse('hide');
 	});
@@ -75,81 +77,120 @@
 }(jQuery));
 
 
+
+
 jQuery(document).ready(function($){
-  $("#hotel-carousel").owlCarousel({
+	topLayerPosition();
+  	$("#hotel-carousel").owlCarousel({
       itemsCustom : [
-[0, 1],
-[450, 1],
-[600, 2],
-[700, 3],
-[1000, 3],
-[1200, 3],
-],
+		[0, 1],
+		[450, 1],
+		[600, 2],
+		[700, 3],
+		[1000, 3],
+		[1200, 3],
+		],
       autoPlay: false,
       pagination: true,
       dots: true
-  });
+	});
 
 
-$("#price-carousel").owlCarousel({
-      itemsCustom : [
-[0, 1],
-[450, 1],
-[600, 2],
-[700, 3],
-[1000, 4],
-[1200, 4],
-],
-      autoPlay: false,
-      pagination: true,
-      dots:true
-  });
-
-
-$("#sponsors-carousel").owlCarousel({
-      itemsCustom : [
-[0, 1],
-[450, 1],
-[600, 2],
-[700, 3],
-[1000, 6],
-[1200, 6],
-],
-      autoPlay: true,
-      pagination: true,
-      dots:true
-  });
-
-
-
-$("#DateCountdown").TimeCircles({
-					"animation": "smooth",
-					"use_background": false,
-					"bg_width": 0,
-					"fg_width": 0,
-
-					"time": {
-						"Days": {
-							"text": "Days",
-							"color": "#fac42b",
-							"show": true
-						},
-						"Hours": {
-							"text": "Hours",
-							"color": "#fac42b",
-							"show": true
-						},
-						"Minutes": {
-							"text": "Minutes",
-							"color": "#fac42b",
-							"show": true
-						},
-						"Seconds": {
-							"text": "Seconds",
-							"color": "#fac42b",
-							"show": true
-						}
-					}
-				});
-
+	$("#price-carousel").owlCarousel({
+	      itemsCustom : [
+			[0, 1],
+			[450, 1],
+			[600, 2],
+			[700, 3],
+			[1000, 4],
+			[1200, 4],
+			],
+	      autoPlay: false,
+	      pagination: true,
+	      dots:true
+	  });
+	
+	
+	$("#sponsors-carousel").owlCarousel({
+	      itemsCustom : [
+			[0, 1],
+			[450, 1],
+			[600, 2],
+			[700, 3],
+			[1000, 6],
+			[1200, 6],
+			],
+	      autoPlay: true,
+	      pagination: true,
+	      dots:true
+	  });
+	
+	
+	
+		$("#DateCountdown").TimeCircles({
+		"animation": "smooth",
+		"use_background": false,
+		"bg_width": 0,
+		"fg_width": 0,
+	
+		"time": {
+			"Days": {
+				"text": "Days",
+				"color": "#fac42b",
+				"show": true
+			},
+			"Hours": {
+				"text": "Hours",
+				"color": "#fac42b",
+				"show": true
+			},
+			"Minutes": {
+				"text": "Minutes",
+				"color": "#fac42b",
+				"show": true
+			},
+			"Seconds": {
+				"text": "Seconds",
+				"color": "#fac42b",
+				"show": true
+			}
+		}
+	});
+	
+	//Top Layer
+	function topLayerPosition() {
+		var topLayerWidth = jQuery('img.top-layer').width();
+		var leftMargin =  "-"+topLayerWidth/2;
+		if (jQuery(window).width() <= 768) {
+			jQuery('img.top-layer').css('margin-left', Number(leftMargin));
+		}
+		blur();
+	}
+	
+	jQuery(window).resize(function() {
+		topLayerPosition();
+	});
+	
+	
+	//Parallax
+	var velocity = 0.05;
+	
+	function update(){ 
+		var pos = $(window).scrollTop(); 
+		$('#slider').each(function() { 
+		    var $element = $(this);
+		    // subtract some from the height b/c of the padding
+		    var height = $element.height();
+		    $(this).css('backgroundPosition', '50%' + Math.round((pos) * velocity) +  'px'); 
+		}); 
+	 };
+	 
+	 if (jQuery(window).width() > 768) {
+	 	$(window).bind('scroll', update);
+	 }
+	 
+	setTimeout(function(){
+		topLayerPosition();
+	}, 2000);
+	 
 });
